288612aa1a73b11ea497f84fd60f07926a8e9f57
HyunChungPark <qkrguscjd2@naver.com>
2020-05-16 21:58:50 +0900

    Make a ChatView using firebase, can receive msg(Basically)

--- app/src/main/java/com/company/baobut2/MyAdapter.java
+++ app/src/main/java/com/company/baobut2/MyAdapter.java
@@ -4,7 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+//
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
